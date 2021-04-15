aa2863443c6a95e0446705207fdcba99904fb146
nik-idc <58989190+nik-idc@users.noreply.github.com>
2021-04-15 10:21:57 +0300

    Update UT_LR2.cpp

--- UT_LR2/UT_LR2.cpp
+++ UT_LR2/UT_LR2.cpp
@@ -15,49 +15,49 @@ namespace UTLR2
 		{
 			srand(time(NULL));
 
-			std::string a;
+			std::string testStr;
 
 			size_t strSize = rand() % 100 + 1, codedSize = 0;
 
 			for (size_t i = 0; i < strSize; i++)
-				a.push_back(rand() % 255);
+				testStr.push_back(rand() % 255);
 
-			sfCode s;
+			sfCode testSF;
 
-			s.encode(a);
+			testSF.encode(testStr);
 
-			std::ifstream f;
-			f.open("coded.txt", std::ifstream::ate | std::ifstream::binary);
-			codedSize = f.tellg();
-			f.close();
+			std::ifstream fileReader;
+			fileReader.open("coded.txt", std::ifstream::ate | std::ifstream::binary);
+			codedSize = fileReader.tellg();
+			fileReader.close();
 
-			Assert::IsTrue(codedSize < a.size());
+			Assert::IsTrue(codedSize < testStr.size());
 		}
 
 		TEST_METHOD(TestDecode)
 		{
 			srand(time(NULL));
 
-			std::string a;
+			std::string testStr;
 
 			size_t strSize = rand() % 100 + 1, codedSize = 0;
 
 			for (size_t i = 0; i < strSize; i++)
-				a.push_back(rand() % 255);
+				testStr.push_back(rand() % 255);
 
-			sfCode s;
+			sfCode testSF;
 
-			s.encode(a);
-			s.decode();
+			testSF.encode(testStr);
+			testSF.decode();
 
 			char ch = 0;
 			std::string decodedStr;
-			std::ifstream f;
-			f.open("decoded.txt", std::ifstream::in | std::ifstream::binary);
-			while (f >> std::noskipws >> ch)
+			std::ifstream fileReader;
+			fileReader.open("decoded.txt", std::ifstream::in | std::ifstream::binary);
+			while (fileReader >> std::noskipws >> ch)
 				decodedStr.push_back(ch);
-			f.close();
-			Assert::IsTrue(decodedStr == a);
+			fileReader.close();
+			Assert::IsTrue(decodedStr == testStr);
 		}
 	};
 }
